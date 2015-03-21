56fb7e50729f88a568e1f686f0f2587f87945f37
kristenmills <kristen@kristen-mills.com>
2015-03-21 19:27:32 -0400

    Short name, short description, and image validations

--- models/event.js
+++ models/event.js
@@ -14,9 +14,26 @@ var Event = Waterline.Collection.extend({
     end_date: 'date',
     description: 'text',
     location: 'string',
-    short_name: 'string',
-    short_description: 'string',
-    image: 'string',
+    short_name: {
+      type: 'string',
+      required: true,
+      minLength: 1,
+      maxLength: 25
+    },
+    short_description: {
+      type: 'string',
+      minLength: 40,
+      maxLength: 70,
+      required: function() {
+        return this.featured;
+      }
+    },
+    image: {
+      type: 'string',
+      required: function() {
+        return this.featured;
+      }
+    },
     featured: 'boolean',
     recurrence: 'string',
 
--- test/models/event.js
+++ test/models/event.js
@@ -5,15 +5,22 @@ var connectToDb = require('../helper');
 var models = {};
 
 var events = require('../fixtures/events');
+var eventParams;
 
 describe('Event', function() {
   connectToDb(models);
 
   context('when everything is present', function(){
-    it('should save', function(){
-      var eventParams = new events.FeaturedEvent();
-      var Event = models.event;
-      var ePromise = Event.create(eventParams);
+    it('featured should save', function(){
+      eventParams = new events.FeaturedEvent();
+      var ePromise =  models.event.create(eventParams);
+
+      return expect(ePromise).to.eventually.be.ok;
+    });
+
+    it('unfeatured should save', function(){
+      eventParams = new events.UnfeaturedEvent();
+      var ePromise =  models.event.create(eventParams);
 
       return expect(ePromise).to.eventually.be.ok;
     });
@@ -22,10 +29,9 @@ describe('Event', function() {
   describe('name', function() {
     context('when not present', function(){
       it('should not save', function(){
-        var eventParams = new events.FeaturedEvent();
+        eventParams = new events.FeaturedEvent();
         eventParams.name = null;
-        var Event = models.event;
-        var ePromise = Event.create(eventParams);
+        var ePromise =  models.event.create(eventParams);
 
         return expect(ePromise).to.eventually.be.rejected;
       });
@@ -33,67 +39,118 @@ describe('Event', function() {
   });
 
   describe('short_name', function() {
+    beforeEach(function(){
+      eventParams = new events.FeaturedEvent();
+    });
+
     context('when not present', function(){
-      it('should not save');
+      it('should not save', function() {
+        eventParams.short_name = null;
+        var ePromise = models.event.create(eventParams);
+
+        return expect(ePromise).to.eventually.be.rejected;
+      });
     });
 
     context('when length is less than 1', function(){
-      it('should not save');
+      it('should not save', function(){
+        eventParams.short_name = '';
+        var ePromise = models.event.create(eventParams);
+
+        return expect(ePromise).to.eventually.be.rejected;
+      });
     });
 
     context('when length is greater than 25', function(){
-      it('should not save');
-    });
+      it('should not save', function(){
+        eventParams.short_name = 'abcdefghijklmnopqrstuvwxyz';
+        var ePromise = models.event.create(eventParams);
 
-    context('when length is between 1 and 25', function(){
-      it('should save');
+        return expect(ePromise).to.eventually.be.rejected;
+      });
     });
   });
 
   describe('short_description', function() {
-    context('when not present', function(){
-      it('should save');
+    beforeEach(function(){
+      eventParams = new events.UnfeaturedEvent();
     });
 
-    context('when length is less than 40', function(){
-      it('should not save');
+    context('when not present', function() {
+      it('should save', function() {
+        eventParams.short_description = null;
+        var ePromise = models.event.create(eventParams);
+
+        return expect(ePromise).to.eventually.be.ok;
+      });
+    });
+
+    context('when length is less than 40', function() {
+      it('should not save', function() {
+        eventParams.short_description = 'something';
+        var ePromise = models.event.create(eventParams);
+
+        return expect(ePromise).to.eventually.be.rejected;
+      });
     });
 
     context('when length is greater than 70', function(){
-      it('should not save');
-    });
+      it('should not save', function() {
+        eventParams.short_description = 'This is an unnecessarily long short description. So long it is insane in the membrane. Neat party trick';
+        var ePromise = models.event.create(eventParams);
 
-    context('when length is between 40 and 70', function(){
-      it('should save');
+        return expect(ePromise).to.eventually.be.rejected;
+      });
     });
   });
 
   describe('featured', function() {
+
     context('when true', function() {
-      context('when short_description is present and image is present', function() {
-        it('should save');
+      beforeEach(function(){
+        eventParams = new events.FeaturedEvent();
       });
 
       context('image is not present', function() {
-        it('should not save');
+        it('should not save', function() {
+          eventParams.image = null;
+          var ePromise = models.event.create(eventParams);
+
+          return expect(ePromise).to.eventually.be.rejected;
+        });
       });
 
       context('when short_description is not present', function() {
-        it('should not save');
+        it('should not save', function() {
+          eventParams.short_description = null;
+          var ePromise = models.event.create(eventParams);
+
+          return expect(ePromise).to.eventually.be.rejected;
+        });
       });
     });
 
     context('when false', function() {
-      context('when short_description is present and image is present', function() {
-        it('should save');
+      beforeEach(function(){
+        eventParams = new events.UnfeaturedEvent();
       });
 
       context('image is not present', function() {
-        it('should save');
+        it('should save', function() {
+          eventParams.image = null;
+          var ePromise = models.event.create(eventParams);
+
+          return expect(ePromise).to.eventually.be.ok;
+        });
       });
 
       context('when short_description is not present', function() {
-        it('should save');
+        it('should save', function() {
+          eventParams.short_description = null;
+          var ePromise = models.event.create(eventParams);
+
+          return expect(ePromise).to.eventually.be.ok;
+        });
       });
     });
   });
