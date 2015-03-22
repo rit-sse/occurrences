b65ab58f3e98160c5ebdc279f98264aff4d6ebbf
kristenmills <kristen@kristen-mills.com>
2015-03-22 12:59:08 -0400

    index and create

--- routes/committees.js
+++ routes/committees.js
@@ -4,10 +4,27 @@ var router = express.Router();
 router
   .route('/')
     .get(function(req, res, next) {
-
+      req.models
+        .committee
+        .find({})
+        .then(function(committees){
+          res.send(committees);
+        })
+        .catch(function(err){
+          next(err)
+        })
     })
     .post(function(req, res, next) {
-
+      req.models
+        .committee
+        .create(req.body.committee)
+        .then(function(committee){
+          res.send(committee);
+        })
+        .catch(function(err){
+          err.status = 422;
+          next(err);
+        })
     });
 
 router
