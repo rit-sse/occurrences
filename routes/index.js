1a54e797f301251dea33cc9fa2397a3e0e796dba
kristenmills <kristen@kristen-mills.com>
2015-03-22 12:29:53 -0400

    Stubbed out event routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var fs = require('fs');
+var path = require('path');
 
 var app = require('../app');
-// console.log(app);
 
 var secret = fs.readFileSync('./secret.key');
 var pub = fs.readFileSync('./secret.pub');
 
 module.exports = function() {
-  app.use('/', require('./home'));
+  app.use('/api/events', require('./events'));
+  app.use(function(req, res, next) {
+    res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+  });
 }
