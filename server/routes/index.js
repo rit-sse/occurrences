0b83e2e83e443dac30c2ee81ea51858fee5b01e4
kristenmills <kristen@kristen-mills.com>
2015-03-29 16:41:37 -0400

    Reorg WOOOOT

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,6 @@ module.exports = function() {
   app.use('/api/events', require('./events'));
   app.use('/api/committees', require('./committees'));
   app.use(function(req, res, next) {
-    res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
   });
 }
