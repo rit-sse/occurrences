7fb3bde8ff737fce82e5cdf3d025299bc15fbed2
kristenmills <kristen@kristen-mills.com>
2015-05-08 19:31:37 -0400

    Oh my god some view stuff. Oddly similar to scoreboard...hmmm

--- app/js/components/routes/layout.jsx
+++ app/js/components/routes/layout.jsx
@@ -4,16 +4,27 @@ var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
 var Link = Router.Link;
 
-var App = React.createClass({
+var Layout = React.createClass({
 
   render() {
     return (
-      <div>
-        <p>I am A layout thing</p>
+      <div className='container'>
+        <div id="header" className="page-header">
+           <div className="h1">
+            <div className="text-left">
+              <Link to="login" className="no-decoration">
+                Occurrences
+              </Link>
+              <small style={{paddingLeft: '10px'}} >
+                SSE Events or Something
+              </small>
+            </div>
+          </div>
+        </div>
         <RouteHandler {...this.props} />
       </div>
     );
   }
 });
 
-module.exports = App;
+module.exports = Layout;
--- app/js/components/routes/login.jsx
+++ app/js/components/routes/login.jsx
@@ -8,9 +8,29 @@ var App = React.createClass({
 
   render() {
     return (
-      <div>
-        <p> I am a page </p>
-      </div>
+      <form className="form-horizontal">
+        <div className="form-group">
+          <label htmlFor="username" className="col-sm-2 control-label">
+            Username
+          </label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" id="username" name="username" placeholder="Username"/>
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password" className="col-sm-2 control-label">
+            Password
+          </label>
+          <div className="col-sm-10">
+            <input type="password" className="form-control" id="password" name="password" placeholder="Password"/>
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-offset-2 col-sm-10">
+            <button className="btn btn-default" type="submit">Sign In</button>
+          </div>
+        </div>
+      </form>
     );
   }
 });
